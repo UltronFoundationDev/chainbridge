8e34a7ef45caa9d616741efea9f1947114a35af1
Michael Pekhota <devops@ultron.foundation>
2022-06-24 18:32:09 +0300

    Change instance type and fix userdata

--- .deployment/src/regional/chainbridge/templates/node-userdata.tpl.sh
+++ .deployment/src/regional/chainbridge/templates/node-userdata.tpl.sh
@@ -1,3 +1,22 @@
+Content-Type: multipart/mixed; boundary="//"
+MIME-Version: 1.0
+
+--//
+Content-Type: text/cloud-config; charset="us-ascii"
+MIME-Version: 1.0
+Content-Transfer-Encoding: 7bit
+Content-Disposition: attachment; filename="cloud-config.txt"
+
+#cloud-config
+cloud_final_modules:
+- [scripts-user, always]
+
+--//
+Content-Type: text/x-shellscript; charset="us-ascii"
+MIME-Version: 1.0
+Content-Transfer-Encoding: 7bit
+Content-Disposition: attachment; filename="userdata.txt"
+
 #!/bin/bash
 export EC2_INSTANCE_ID="$(curl -s http://169.254.169.254/latest/meta-data/instance-id)"
 export IP="$(curl -s http://169.254.169.254/latest/meta-data/public-ipv4)"
